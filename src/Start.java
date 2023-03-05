c3e7c2bfac31819a0442f0a08da58a9509a11e05
Imad Maachou <109520023+imad7quasar@users.noreply.github.com>
2023-03-05 21:09:29 +0100

    adding the src file

---
+++ src/Start.java
@@ -0,0 +1,46 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+// printing Hello world 
+		
+System.out.println("Hello World ") ; 
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
